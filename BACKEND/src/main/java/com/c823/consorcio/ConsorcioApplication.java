f956cf0ee9135251d4b3014347c87ab9e13fafe0
agustin-kiryk <agustin.kiryk@gmail.com>
2022-11-15 19:36:01 -0300

    Initial Commit Backend

---
+++ BACKEND/src/main/java/com/c823/consorcio/ConsorcioApplication.java
@@ -0,0 +1,13 @@
+package com.c823.consorcio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsorcioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsorcioApplication.class, args);
+	}
+
+}
