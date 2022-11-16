aa27a9b8558fc2474040c88eeeff0ae3ab6f07b8
Miguel Angel Ochoa <miguel.angel.8a.rivera@gmail.com>
2022-11-16 10:57:21 -0500

    SideBar added in proyect, and react-icons install

--- FRONTEND/condominios/src/App.tsx
+++ FRONTEND/condominios/src/App.tsx
@@ -1,41 +1,93 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import {
+  RiBuilding4Line,
+  RiDashboardLine,
+  RiMessage3Line,
+  RiWallet2Line,
+  RiCommunityFill,
+  RiAlertFill,
+  RiLogoutBoxLine,
+} from 'react-icons/ri';
+import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <div className="App">
-            <div>
-                <h1 className="text-3xl bg-red-600">
-                    Texto de prueba TailwindCSS
-                </h1>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+  return (
+    <div className="min-h-screen grid grid-cols-6">
+      <div className=" col-span-1 p-8 ">
+        {/* Logotipo */}
+        <div className="flex flex-row text-center p-8">
+          <h1 className="flex flex-row gap-4 text-center uppercase font-bold tracking-[4px]">
+            {' '}
+            <RiBuilding4Line />
+            liveto
+          </h1>
         </div>
-    );
+        <div className=" flex flex-col justify-between h-[550px]">
+          {/* Menu */}
+          <nav>
+            <ul>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center gap-4 font-semibold hover:bg-blue-300 p-2 hover:font-bold rounded-lg transition-colors"
+                >
+                  <RiDashboardLine />
+                  Escritorio
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center gap-4 font-semibold hover:bg-blue-300 p-2 hover:font-bold rounded-lg transition-colors"
+                >
+                  <RiWallet2Line />
+                  Gastos y expensas
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center gap-4 font-semibold hover:bg-blue-300 p-2 hover:font-bold rounded-lg transition-colors"
+                >
+                  <RiCommunityFill />
+                  Espacios comunes
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center gap-4 font-semibold hover:bg-blue-300 p-2 hover:font-bold rounded-lg transition-colors"
+                >
+                  <RiMessage3Line />
+                  Mensajes
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center gap-4 font-semibold hover:bg-blue-300 p-2 hover:font-bold rounded-lg transition-colors"
+                >
+                  <RiAlertFill />
+                  Reportes
+                </a>
+              </li>
+            </ul>
+          </nav>
+          {/* Boton de cerrar sesion */}
+          <div>
+            <a
+              href="#"
+              className="flex items-center gap-4 font-semibold hover:bg-blue-300 p-2 hover:font-bold rounded-lg transition-colors"
+            >
+              <RiLogoutBoxLine />
+              Salir de cuenta
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className=" col-span-5">Hola2</div>
+    </div>
+  );
 }
 
 export default App;
